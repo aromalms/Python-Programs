f61e11f7d928305385c5c847dcc93998db3ceab2
Aromal M S <aromalms3318@gmail.com>
2025-01-22 20:55:56 +0530

    first commit

--- aliasing.py
+++ aliasing.py
@@ -3,7 +3,8 @@ list1=[10,23,55]
 list2=list1         #refers to same memory location 
 print({id(list1)})
 print({id(list2)})
-
+print("list 1: ",list1)
+print("list 2: ",list2) 
 print("Changing element '23' of List 1")
 list1[1]=99
 print(list1)
