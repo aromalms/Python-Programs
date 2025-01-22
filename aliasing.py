f1a3677e32678e201aa0e3f83bbe4c7b80536bfc
Aromal M S <aromalms3318@gmail.com>
2025-01-22 20:48:23 +0530

    first commit

---
+++ aliasing.py
@@ -0,0 +1,10 @@
+#Program to show aliasing and side effects
+list1=[10,23,55]
+list2=list1         #refers to same memory location 
+print({id(list1)})
+print({id(list2)})
+
+print("Changing element '23' of List 1")
+list1[1]=99
+print(list1)
+print(list2)        #automatically element '23' in list2 also changes
