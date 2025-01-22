62a39ea2b205b18a1214cb2e6d442e8be5957cac
Aromal M S <aromalms3318@gmail.com>
2025-01-22 20:43:16 +0530

    first commit

--- transpose.py
+++ transpose.py
@@ -1,10 +1,10 @@
+#Program to transpose a matrix
 matrix=[
     [1,3,5,3],
     [3,6,7,4],
     [4,8,3,6],
 ]
 transposed=[]
-
 for i in range(4):
     transposed.append([row[i] for row in matrix])
 print(transposed)
