9dac06c71246740d4720925c7c4cdf1a85be33e9
Aromal M S <aromalms3318@gmail.com>
2025-01-24 07:49:44 +0530

    commit

---
+++ dictionary.py
@@ -0,0 +1,10 @@
+info={}
+info["name"]="aromal"
+info["place"]="pala"
+info["college"]="sjcet"
+print(info)
+#accessing using key
+print("Acessing using key 'name': ",info["name"])
+#removing
+print(info.pop("name"))
+print("After removal of name",info)
