8113539ef7134c6ee15a7eb4ec60959d091a59ba
Aromal M S <aromalms3318@gmail.com>
2025-01-27 07:48:10 +0530

    commit

---
+++ caseconv.py
@@ -0,0 +1,9 @@
+#Capitalize first letter
+str1="hello"
+print(str1.capitalize())
+#lowercase all letters
+str2="HELLO"
+print(str2.lower())
+#Capitalize lowercase letters and vice versa
+str3="HeLLo"
+print(str3.swapcase())
