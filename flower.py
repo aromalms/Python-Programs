d1e5beccdd012f19d581e140757e574071d058c3
Aromal M S <aromalms3318@gmail.com>
2025-04-30 22:30:59 +0530

    commit

--- flower.py
+++ flower.py
@@ -1,13 +1,13 @@
 #draw a circle
 import turtle
 x=turtle.Turtle()
-x.speed(0)
+x.speed(4)
 for i in range(5):
     x.fillcolor("red")
     x.begin_fill()
-    x.circle(190,90)
+    x.circle(100,90)
     x.left(90)
-    x.circle(190,90)
-    x.left(18)
+    x.circle(100,90)
+    x.left(20)
     x.end_fill()
 turtle.done()
