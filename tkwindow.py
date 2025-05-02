50e338acbc218eb206f35753d9c88d1e9384992e
Aromal M S <aromalms3318@gmail.com>
2025-05-02 22:55:01 +0530

    commit

---
+++ tkwindow.py
@@ -0,0 +1,12 @@
+#tkinter window with 4 buttons
+from tkinter import *
+parent = Tk()
+redbutton = Button(parent, text = "Red", fg = "red")
+redbutton.pack( side = LEFT)
+greenbutton = Button(parent, text = "Green", fg = "green")
+greenbutton.pack( side = RIGHT )
+bluebutton = Button(parent, text = "Blue", fg = "blue")
+bluebutton.pack( side = TOP )
+blackbutton = Button(parent, text = "Black", fg = "black")
+blackbutton.pack( side = BOTTOM)
+parent.mainloop()
